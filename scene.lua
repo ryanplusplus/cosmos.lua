64eb080f936bba056f7bf5cb2aa6833fc6d4a4be
Ryan Hartlage <ryanplusplus@gmail.com>
2014-12-22 09:41:01 -0500

    Scenes now use closures to hide internal state

--- scene.lua
+++ scene.lua
@@ -1,5 +1,5 @@
-local scene_api = {}
-scene_api.__index = scene_api
+-- local scene_api = {}
+-- scene_api.__index = scene_api
 
 function remove_value(t, value)
   local to_remove = {}
@@ -16,68 +16,66 @@ function remove_value(t, value)
 end
 
 function Scene()
-  local scene = {
-    render_systems = {},
-    update_systems = {},
-    _entities = {}
-  }
+  local render_systems = {}
+  local update_systems = {}
+  local entities = {}
 
-  return setmetatable(scene, scene_api)
-end
+  return {
+    new_entity = function()
+      local entity = Entity()
+      entities[entity] = true
+      return entity
+    end,
 
-function scene_api:new_entity()
-  local entity = Entity()
-  self._entities[entity] = true
-  return entity
-end
+    entities = function()
+      return entities
+    end,
 
-function scene_api:entities()
-  return self._entities
-end
+    entities_with = function(self, ...)
+      local entities_with = {}
+      local components = table.pack(...)
 
-function scene_api:entities_with(...)
-  local entities_with = {}
-  local components = table.pack(...)
+      for entity in pairs(entities) do
+        local has_all = true
 
-  for entity in pairs(self._entities) do
-    local has_all = true
+        for _, component in ipairs(components) do
+          if not entity[component] then has_all = false end
+        end
 
-    for _, component in ipairs(components) do
-      if not entity[component] then has_all = false end
+        if has_all then entities_with[entity] = true end
+      end
+
+      return entities_with
+    end,
+
+    add_update_system = function(self, update_system)
+      table.insert(update_systems, update_system)
+    end,
+
+    remove_update_system = function(self, update_system)
+      remove_value(update_systems, update_system)
+    end,
+
+    update = function(self, dt)
+      for _, update_system in ipairs(update_systems) do
+        update_system(self, dt)
+      end
+    end,
+
+    add_render_system = function(self, render_system)
+      table.insert(render_systems, render_system)
+    end,
+
+    remove_render_system = function(self, render_system)
+      remove_value(render_systems, render_system)
+    end,
+
+    render = function(self)
+      for _, render_system in ipairs(render_systems) do
+        render_system(self)
+      end
     end
-
-    if has_all then entities_with[entity] = true end
-  end
-
-  return entities_with
-end
-
-function scene_api:add_update_system(update_system)
-  table.insert(self.update_systems, update_system)
-end
-
-function scene_api:remove_update_system(update_system)
-  remove_value(self.update_systems, update_system)
-end
-
-function scene_api:update(dt)
-  for _, update_system in ipairs(self.update_systems) do
-    update_system(self, dt)
-  end
-end
-
-function scene_api:add_render_system(render_system)
-  table.insert(self.render_systems, render_system)
-end
-
-function scene_api:remove_render_system(render_system)
-  remove_value(self.render_systems, render_system)
-end
-
-function scene_api:render()
-  for _, render_system in ipairs(self.render_systems) do
-    render_system(self)
-  end
+  }
 end
 
 entity_api = {}
